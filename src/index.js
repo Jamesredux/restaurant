363eccc84156c85a449d5edd09c4b47a245a66a1
James McCann <jamesredux@gmail.com>
2020-07-31 12:41:26 +0100

    server setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("testing 123")
+
+console.log("another test");
+console.log("maybest");
+
+const content = document.querySelector('.test');
+const newPara = document.createElement('p')
+const para = `<p>This para</p>`;
+newPara.innerHTML = para;
+content.appendChild(newPara);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const path = require('path');
 
 module.exports = {
@@ -5,5 +8,19 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          filename: './dist/index.html',
+          template: './dist/index.html'
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+      ]
 }
